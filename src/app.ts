4cb544cb9db2a5e4d7b8da68ba3be2986ad5e858
tmraaex <tmr.aaex@gmail.com>
2025-02-02 04:22:27 +0100

    added cors

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,14 @@
 import express, { Express } from "express";
 import baseRoutes from "@routes/index";
 import dotenv from "dotenv";
-
+import cors from "cors";
 dotenv.config();
 
 export const app: Express = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 //routes
 app.use("/", baseRoutes);
