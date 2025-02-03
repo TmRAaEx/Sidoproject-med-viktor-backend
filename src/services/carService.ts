03472772f596f4d383719816406599a0f71ca55b
TmRAaEx <tmr.aaex@gmail.com>
2025-02-03 17:01:05 +0100

    Added api endpoint to get all cars from database

--- src/services/carService.ts
+++ src/services/carService.ts
@@ -2,21 +2,25 @@
 
 import Car from "@models/car";
 import { ICar } from "@interfaces/Icar";
-import {fetchAndConvertLecabData} from "@services/lecabService"
+import { fetchAndConvertLecabData } from "@services/lecabService";
 
 export const getCarById = async (carId: string): Promise<ICar | null> => {
-    return Car.findById(carId)
-}
+  return Car.findById(carId);
+};
+
+export const getCars = async (limit: number): Promise<ICar[]> => {
+  return Car.find().limit(limit);
+};
 
 export const saveCarsToDB = async () => {
-    const convertedCars = await fetchAndConvertLecabData();
-    for (const car of convertedCars) {
-        const existingCar = await Car.find({sku: car.sku, id: car.id})
-        if (existingCar.length > 0) {
-            console.log("Car already exists", existingCar)
-            continue;
-        }
-        const newCar = await Car.create(car);
-        console.log("Car created: ", newCar);
+  const convertedCars = await fetchAndConvertLecabData();
+  for (const car of convertedCars) {
+    const existingCar = await Car.find({ sku: car.sku, id: car.id });
+    if (existingCar.length > 0) {
+      console.log("Car already exists", existingCar);
+      continue;
     }
-}
+    const newCar = await Car.create(car);
+    console.log("Car created: ", newCar);
+  }
+};
